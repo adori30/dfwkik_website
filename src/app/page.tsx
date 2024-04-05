83dc545f9523ccf4aa5798ffe109b3e7526223c0
Adria Navarro <adria.navarro.martin@gmail.com>
2024-04-05 15:54:02 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,147 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
+  return (
+    // <div className="flex min-h- bg-cover bg-center bg-[url('../../public/kamae.jpg')] items-center justify-center">hello there</div>
+    <div className="flex flex-col">
+      <Header />
+      <div className="text-center p-12 text-xl">Our disciplines</div>
+      <Disciplines />
+    </div>
+  );
+}
+
+function Disciplines() {
+  return (
+    <div className="flex min-h-96 min-w-max md:flex-row flex-col">
+      <DisciplineTile
+        name="Kendo"
+        description="Japanese martial art emphasizing swordsmanship."
+        backgroundImg="kendo_pic.jpg"
+      />
+      <DisciplineTile
+        name="Iaido"
+        description="Discipline focusing on the art of drawing the sword."
+        backgroundImg="iaido_pic.jpg"
+      />
+      <DisciplineTile
+        name={"Jodo"}
+        description="Traditional Japanese staff fighting technique."
+        backgroundImg="jodo_pic.jpg"
+      />
+    </div>
+  );
+}
+
+function DisciplineTile({
+  name,
+  description,
+  backgroundImg,
+}: {
+  name: string;
+  description: string;
+  backgroundImg: string;
+}) {
+  return (
+    // <div
+    //   className="flex-1 bg-cover bg-center m-4 transition hover:bg-slate-500"
+    //   style={{ backgroundImage: `url('/${backgroundImg}')` }}
+    // >
+    //   <div className="text-center content-center hover:-translate-y-1/4 transition w-full h-full text-5x">
+    //     {name}
+    //   </div>
+    // </div>
+
+    <div
+      className="relative flex-1 bg-cover bg-center m-4 transition-all hover:pb-28  text-5xl cursor-pointer"
+      style={{ backgroundImage: `url('/${backgroundImg}')` }}
+    >
+      <div className="absolute flex flex-col items-center justify-center w-full h-full z-10 text-center content-center hover:pb-28 transition-all hover:bg-black hover:bg-opacity-50">
+        {name}
+        <div className="absolute w-full h-full opacity-0 hover:opacity-100 transition-all flex items-center justify-center mt-28 text-xl">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav className="bg-white shadow">
+        <div className="container mx-auto px-6 py-3 ">
+          <div className="md:flex md:items-center md:justify-between">
+            <div className="flex justify-between items-center">
+              <div className="text-xl font-semibold text-gray-700">
+                <a
+                  href="#"
+                  className="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl"
+                >
+                  Dallas/Forth Worth Kendo & Iaido Kyokai
+                </a>
+              </div>
+
+              <div className="flex md:hidden">
+                <button
+                  type="button"
+                  className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+                  aria-label="toggle menu"
+                >
+                  <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                    <path
+                      fill-rule="evenodd"
+                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+
+            <div className="hidden -mx-4 md:flex md:items-center">
+              <a
+                href="#"
+                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
+              >
+                Web developers
+              </a>
+              <a
+                href="#"
+                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
+              >
+                Web Designers
+              </a>
+              <a
+                href="#"
+                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
+              >
+                UI/UX Designers
+              </a>
+              <a
+                href="#"
+                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="w-full bg-cover bg-center bg-[url('../../public/kamae.jpg')] h-96">
+        <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+          <div className="text-center">
+            <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl">
+              Dallas/Fort Worth Kendo & Iaido Kyokai
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
