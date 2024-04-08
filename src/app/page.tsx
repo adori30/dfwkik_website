a57aae4a408aca9d4fee0d3b1435cb1510074d9e
Adria Navarro <adria.navarro.martin@gmail.com>
2024-04-08 16:39:21 -0500

    add responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import Zoom from 'react-medium-image-zoom'
 import WhereToFindUs from "./where-to-find-us";
 import { ReactNode } from "react";
 
@@ -8,13 +7,14 @@ export default function HomePage() {
   return (
     // <div className="flex min-h- bg-cover bg-center bg-[url('../../public/kamae.jpg')] items-center justify-center">hello there</div>
     <TextureBackground>
+      <HeaderPicture />
       <div className="flex flex-col ">
         <Header />
-        <div className="bg-slate-300 p-12 text-center text-4xl font-thin text-gray-950">
+        <div className="bg-slate-300 p-12 text-center md:text-4xl text-2xl font-thin text-gray-950">
           Our disciplines
         </div>
         <Disciplines />
-        <div className="p-12 text-center text-4xl font-thin">
+        <div className="p-12 text-center md:text-4xl text-2xl font-thin">
           Where to find us
         </div>
         <WhereToFindUs />
@@ -25,11 +25,56 @@ export default function HomePage() {
 
 function TextureBackground({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="pattern-cross pattern-slate-600 pattern-bg-slate-700 pattern-size-8 pattern-opacity-100 bg-slate-700">
+    <div className="pattern-cross pattern-slate-600 pattern-bg-slate-700 pattern-size-8 pattern-opacity-100 bg-slate-700 h-full">
       {children}
     </div>
   );
 }
+
+function HeaderPicture() {
+  return (
+    <div className="absolute md:w-[82rem] md:h-[60rem] right-0 z-0 md:-top-60 w-full h-96">
+      <Image
+        src={"/kamae_clipped.png"}
+        fill
+        objectFit="cover"
+        alt="picture of a kendoka"
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="flex h-96 items-center justify-center ">
+      <div className="container absolute left-8 top-8 flex flex-row items-center">
+        <Image
+          src={"/logo_color.png"}
+          width={30}
+          height={30}
+          alt="logo"
+          className="mr-2"
+        />
+        <div className="font-sans md:text-2xl font-extralight">
+          Dallas/Fort Worth Kendo & Iaido Kyokai
+        </div>
+      </div>
+
+      <div className="relative z-10 md:mr-28 flex flex-col items-center">
+        <div className="md:text-3xl font-light p-4 text-xl">
+          Learn the arts of Japanese swordsmanship at the oldest dojo in DFW
+        </div>
+        <div className="md:text-xl font-thin">Established 1976</div>
+        <HeaderButton className="mt-4">
+          <Link href="https://www.dfwkik.org/dallas/membership/">
+            Join us now
+          </Link>
+        </HeaderButton>
+      </div>
+    </div>
+  );
+}
+
 
 function Disciplines() {
   return (
@@ -64,12 +109,12 @@ function DisciplineTile({
 }>) {
   return (
     <div
-      className="relative m-4 flex-1 cursor-pointer bg-cover bg-center text-5xl  transition-all hover:pb-28"
+      className="relative m-4 md:flex-1 h-72 cursor-pointer bg-cover bg-center text-5xl  transition-all md:hover:pb-28"
       style={{ backgroundImage: `url('/${backgroundImg}')` }}
     >
-      <div className="absolute z-10 flex h-full w-full flex-col content-center items-center justify-center text-center transition-all hover:bg-black hover:bg-opacity-50 hover:pb-28">
+      <div className="absolute z-10 flex h-full w-full flex-col content-center items-center justify-center text-center transition-all md:hover:bg-black md:hover:bg-opacity-50 md:hover:pb-28">
         {name}
-        <div className="absolute mt-28 flex h-full w-full items-center justify-center text-xl opacity-0 transition-all hover:opacity-100">
+        <div className="absolute mt-28 flex h-full w-full items-center justify-center text-xl md:opacity-0 transition-all md:hover:opacity-100 opacity-100">
           {description}
         </div>
       </div>
@@ -87,119 +132,5 @@ function HeaderButton({
     >
       {children}
     </div>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      {/* <nav className="bg-white shadow">
-        <div className="container mx-auto px-6 py-3 ">
-          <div className="md:flex md:items-center md:justify-between">
-            <div className="flex justify-between items-center">
-              <div className="text-xl font-semibold text-gray-700">
-                <a
-                  href="#"
-                  className="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl"
-                >
-                  Dallas/Forth Worth Kendo & Iaido Kyokai
-                </a>
-              </div>
-
-              <div className="flex md:hidden">
-                <button
-                  type="button"
-                  className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
-                  aria-label="toggle menu"
-                >
-                  <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                    <path
-                      fill-rule="evenodd"
-                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-
-            <div className="hidden -mx-4 md:flex md:items-center">
-              <a
-                href="#"
-                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-              >
-                Web developers
-              </a>
-              <a
-                href="#"
-                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-              >
-                Web Designers
-              </a>
-              <a
-                href="#"
-                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-              >
-                UI/UX Designers
-              </a>
-              <a
-                href="#"
-                className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-              >
-                Contact
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav> */}
-
-      <div className="relative flex h-96 items-center justify-center ">
-        <Image
-          src={"/kamae_clipped.png"}
-          width={1500}
-          height={500}
-          alt="picture of a kendoka"
-          style={{
-            objectFit: "none",
-            position: "absolute",
-            top: -300,
-            right: 0,
-          }}
-        />
-        <div className="container absolute left-8 top-8 flex flex-row items-center">
-          <Image
-            src={"/logo_color.png"}
-            width={30}
-            height={30}
-            alt="logo"
-            className="mr-2"
-          />
-          <div className="font-sans text-2xl font-extralight">
-            Dallas/Fort Worth Kendo & Iaido Kyokai
-          </div>
-        </div>
-
-        <div className="relative z-10 mr-28 flex flex-col items-center">
-          <div className="text-3xl font-light">
-            Learn the arts of Japanese swordsmanship at the oldest dojo in DFW
-          </div>
-          <div className="text-xl font-thin">Established 1976</div>
-          <HeaderButton className="mt-4">
-            <Link href="https://www.dfwkik.org/dallas/membership/">
-              Join us now
-            </Link>
-          </HeaderButton>
-        </div>
-      </div>
-
-      {/* <div className="w-full bg-right bg-cover bg-no-repeat bg-[url('../../public/kamae_clipped.png')] h-96 pr-48">
-        <div className="flex items-center justify-center h-full w-full">
-          <div className="text-center">
-            <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl">
-              Dallas/Fort Worth Kendo & Iaido Kyokai
-            </h1>
-          </div>
-        </div>
-      </div> */}
-    </header>
   );
 }
--- src/app/where-to-find-us.tsx
+++ src/app/where-to-find-us.tsx
@@ -7,27 +7,27 @@ import "react-medium-image-zoom/dist/styles.css";
 
 export default function WhereToFindUs() {
   return (
-    <div className="flex h-96 flex-col justify-between md:flex-row mr-4">
-      <div className="bg-pi w-1/2 content-center pl-16 pr-16 text-2xl font-extralight">
+    <div className="m-4 flex md:h-96 flex-col justify-between md:flex-row">
+      <div className="content-center pl-16 pr-16 text-xl font-extralight md:w-1/2 md:text-2xl">
         <div>
-          We practice at Greenhill School. It is located on the North West corner of of Midway
-          and Spring Valley. Practice is held in the Main Gymnasium (Indicated
-          by H), best accessed from the Hornet Drive entrance off of Midway
-          (Indicated by O).
+          We practice at Greenhill School. It is located on the North West
+          corner of of Midway and Spring Valley. Practice is held in the Main
+          Gymnasium (Indicated by H), best accessed from the Hornet Drive
+          entrance off of Midway (Indicated by O).
         </div>
         <div className="mt-12 underline">
-          <Link href={"https://goo.gl/maps/Ayx912EAdx5qYHQbA"}>
+          <Link href={"https://maps.app.goo.gl/D13em6GkePDxQCyT7"}>
             4141 Spring Valley Rd. Addison, Texas 75001
           </Link>
         </div>
       </div>
-      <div className="h-96 w-1/2 justify-end content-end flex flex-row">
+      <div className="flex md:h-96 flex-row content-end md:justify-end md:w-1/2 justify-center">
         <Zoom>
           <img
-          alt="Map of the dojo location"
-          src="/greenhill_map.jpg"
-          className="h-96"
-        />
+            alt="Map of the dojo location"
+            src="/greenhill_map.jpg"
+            className="md:h-96 h-44 m-4"
+          />
         </Zoom>
       </div>
     </div>
