b791fe767c2c47be3f2f8062141c0fb15ef685fe
Adria Navarro <adria.navarro.martin@gmail.com>
2024-04-09 14:01:12 -0500

    add disciplines folder

---
+++ src/app/disciplines/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import markdownToHtml from "@/lib/markdown-to-html";
+import { getDocumentBySlug } from "outstatic/server";
+
+async function getData(params: { slug: string }) {
+  const discipline = getDocumentBySlug("Content", 'kendo', [
+    "title",
+    "content",
+  ]);
+
+console.log(discipline)
+
+  const content = await markdownToHtml(discipline?.content || "");
+
+
+  return {
+    title: discipline?.title,
+    content
+  }
+}
+
+export default async function DisciplinePage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data = await getData(params);
+  return (
+    <div>
+      <div>Content for {params.slug}</div>
+      <div>Title: {data.title}</div>
+      <div>{data.content}</div>
+    </div>
+  );
+}
